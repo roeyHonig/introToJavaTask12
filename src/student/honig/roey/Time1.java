6e905782669a67655ab8612b600c79829fc380a5
Roey Honig <roeyhonig94@gmail.com>
2020-04-25 15:43:33 +0300

    Time1 + Time2 class API

--- src/student/honig/roey/Time1.java
+++ src/student/honig/roey/Time1.java
@@ -1,5 +1,10 @@
 package student.honig.roey;
 
+/** Represents time - hours:minutes.
+ * @author Roey Honig
+ * @author ID: roeyhonig94@gmail.com
+ * @author ID: 060873940
+ */
 public class Time1 {
 
     private int _hour; // 0-23
@@ -13,7 +18,10 @@ public class Time1 {
     public final int HOUR_PER_DAY = 24;
     public final int MINUTE_PER_DAY = HOUR_PER_DAY * MINUTE_PER_HOUR;
 
-    // Constructors
+    /** Constructs a Time1 object.
+     * @param hour Integer representing the Hour.
+     * @param minute Integer representing the Minute.
+     */
     public Time1(int hour, int minute) {
         _hour = hour;
         _minute = minute;
@@ -25,21 +33,31 @@ public class Time1 {
         }
     }
 
+    /**Copy constructor for Time1.
+     * @param t Time1 object to be copied into a new Time1 instance.
+     */
     public Time1 (Time1 t){
         _hour = t.getHour();
         _minute = t.getMinute();
     }
 
-    // getters
+    /** Returns the hour of the time.
+     * @return The hour of the time
+     */
     public int getHour() {
         return _hour;
     }
 
+    /** Returns the minutes of the time.
+     * @return The minutes of the time
+     */
     public int getMinute() {
         return _minute;
     }
 
-    // setters
+    /** Changes the hour of the time. If an illegal number is received hour will be unchanged.
+     * @param num - The new hour
+     */
     public void setHour(int num) {
         if (num < HOUR_MIN_VALUE || num > HOUR_MAX_VALUE) {
             return;
@@ -47,6 +65,9 @@ public class Time1 {
         _hour = num;
     }
 
+    /** Changes the minute of the time. If an illegal number is received minute will be unchanged.
+     * @param num - The new minute
+     */
     public void setMinute(int num) {
         if (num < MINUTE_MIN_VALUE || num > MINUTE_MAX_VALUE) {
             return;
@@ -54,19 +75,33 @@ public class Time1 {
         _minute = num;
     }
 
+    /** Returns a string representation of this time ("hh:mm").
+     * @return String representation of this time ("hh:mm").
+     */
     public String toString() {
         return ""+(_hour < 10 ? "0"+_hour : _hour)+":"+(_minute < 10 ? "0"+_minute : _minute);
     }
 
+    /** Return the amount of minutes since midnight.
+     * @return amount of minutes since midnight.
+     */
     public int minFromMidnight(){
         return  MINUTE_PER_HOUR*_hour+ _minute;
     }
 
+    /** Checks if the received time is equal to this time.
+     * @param  other The time to be compared with this time
+     * @return true if the received time is equal to this time
+     */
     public boolean equals(Time1 other) {
         if (this == other) return true;
         return _hour == other.getHour() && _minute == other.getMinute();
     }
 
+    /** Checks if this time is before a received time.
+     * @param  other The time to check if this time is before
+     * @return true if this time is before other time
+     */
     public boolean before (Time1 other){
         if (this.minFromMidnight() <= other.minFromMidnight()) {
             return true;
@@ -74,16 +109,28 @@ public class Time1 {
         return false;
     }
 
+    /** Checks if this time is after a received time.
+     * @param  other The time to check if this time is after
+     * @return true if this time is after other time
+     */
     public boolean after (Time1 other){
         return other.before(this);
     }
 
+    /** Calculates the difference (in minutes) between two times. Assumption: this time is after other time.
+     * @param  other The time to check the difference to
+     * @return int difference in minutes
+     */
     public int difference(Time1 other){
         int thisMinFromMidnight = this.minFromMidnight();
         int otherMinFromMidnight = other.minFromMidnight();
         return thisMinFromMidnight-otherMinFromMidnight;
     }
 
+    /** Adds num Minutes to time.
+     * @param  num The number of minutes to add
+     * @return the update time
+     */
     public Time1 addMinutes(int num){
             int totalMinute = num + this.minFromMidnight();
             int x = Math.abs(totalMinute) % MINUTE_PER_DAY;
--- src/student/honig/roey/Time2.java
+++ src/student/honig/roey/Time2.java
@@ -1,5 +1,10 @@
 package student.honig.roey;
 
+/** Represents time by the minutes from midnight. Values must represent a proper time.
+ * @author Roey Honig
+ * @author ID: roeyhonig94@gmail.com
+ * @author ID: 060873940
+ */
 public class Time2 {
 
     private int _minFromMid;
@@ -12,7 +17,10 @@ public class Time2 {
     public final int HOUR_PER_DAY = 24;
     public final int MINUTE_PER_DAY = HOUR_PER_DAY * MINUTE_PER_HOUR;
 
-    // Constructors
+    /** Constructs a Time2 object. Construct a new time instance with the specified hour and minute . hour should be between 0-23, otherwise it should be set to 0. minute should be between 0-59, otherwise they should be set to 0.
+     * @param hour - hour
+     * @param minute - minute
+     */
     public Time2(int hour, int minute) {
         int _hour = hour;
         int _minute = minute;
@@ -25,20 +33,30 @@ public class Time2 {
         _minFromMid = MINUTE_PER_HOUR  * _hour + _minute;
     }
 
+    /** Copy constructor for Time2. Constructs a time with the same variables as another time.
+     * @param t - The time object from which to construct the new time
+     */
     public Time2 (Time2 t){
         _minFromMid = t.minFromMidnight();
     }
 
-    // getters
+    /** Returns the hour of the time.
+     * @return The hour of the time
+     */
     public int getHour() {
         return (_minFromMid-(_minFromMid % MINUTE_PER_HOUR))/MINUTE_PER_HOUR;
     }
 
+    /** Returns the minutes of the time.
+     * @return The minutes of the time
+     */
     public int getMinute() {
         return _minFromMid % MINUTE_PER_HOUR;
     }
 
-    // setters
+    /** Changes the hour of the time. If an illegal number is received hour will be unchanged.
+     * @param num - The new hour
+     */
     public void setHour(int num) {
         if (num < HOUR_MIN_VALUE || num > HOUR_MAX_VALUE) {
             return;
@@ -46,6 +64,9 @@ public class Time2 {
         _minFromMid = MINUTE_PER_HOUR * num + getMinute();
     }
 
+    /** Changes the minute of the time. If an illegal number is received minute will be unchanged.
+     * @param num - The new minute
+     */
     public void setMinute(int num) {
         if (num < MINUTE_MIN_VALUE || num > MINUTE_MAX_VALUE) {
             return;
@@ -53,21 +74,35 @@ public class Time2 {
         _minFromMid = MINUTE_PER_HOUR * getHour() + num;
     }
 
+    /** Returns a string representation of this time ("hh:mm").
+     * @return String representation of this time ("hh:mm").
+     */
     public String toString() {
         int _hour = getHour();
         int _minute = getMinute();
         return ""+(_hour < 10 ? "0"+_hour : _hour)+":"+(_minute < 10 ? "0"+_minute : _minute);
     }
 
+    /** Return the amount of minutes since midnight.
+     * @return amount of minutes since midnight.
+     */
     public int minFromMidnight(){
         return  _minFromMid;
     }
 
+    /** Checks if the received time is equal to this time.
+     * @param  other The time to be compared with this time
+     * @return true if the received time is equal to this time
+     */
     public boolean equals(Time2 other) {
         if (this == other) return true;
         return _minFromMid == other.minFromMidnight();
     }
 
+    /** Checks if this time is before a received time.
+     * @param  other The time to check if this time is before
+     * @return true if this time is before other time
+     */
     public boolean before (Time2 other){
         if (_minFromMid <= other.minFromMidnight()) {
             return true;
@@ -75,14 +110,26 @@ public class Time2 {
         return false;
     }
 
+    /** Checks if this time is after a received time.
+     * @param  other The time to check if this time is after
+     * @return true if this time is after other time
+     */
     public boolean after (Time2 other){
         return other.before(this);
     }
 
+    /** Calculates the difference (in minutes) between two times. Assumption: this time is after other time.
+     * @param  other The time to check the difference to
+     * @return int difference in minutes
+     */
     public int difference(Time2 other){
         return _minFromMid-other.minFromMidnight();
     }
 
+    /** Adds num Minutes to time.
+     * @param  num The number of minutes to add
+     * @return the update time
+     */
     public Time2 addMinutes(int num){
         int totalMinute = num + this.minFromMidnight();
         int x = Math.abs(totalMinute) % MINUTE_PER_DAY;
